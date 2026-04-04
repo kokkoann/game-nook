525c0d78a92013101196fbb1c8f95b0416671586
kokkoann <maryann162002@gmail.com>
2026-04-03 22:47:32 -0600

    response testing

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-document.body.innerHTML = "<h1>Game Nook 🎮</h1>";
+//document.body.innerHTML = "<h1>Game Nook 🎮</h1>";
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+document.body.appendChild(canvas);
+
+//a ver si se ve algo xd
+ctx.fillStyle = "purple";
+ctx.fillRect(100, 100, 200, 250);
